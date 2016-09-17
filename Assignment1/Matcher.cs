5adb20272b879b90d5b432314fd1c3fd5893de56
JoshuaWhitney <joshua.whitney@aggiemail.usu.edu>
2016-09-16 22:56:01 -0600

    Code Complete
    Program code for assignment 1 is complete (sans 2 matcher alogirthms).
    UML diagrams are compelte, stored elsewhere. About to implment unit
    tests.

---
+++ Assignment1/Matcher.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Matcher
+    {
+        private bool checkIfSSN(Person p1, Person p2)
+        {
+            if (p1.SocialSecurityNumber == null || p2.SocialSecurityNumber == null) return false;
+            return true;
+        }
+
+        private bool checkIfSFN(Person p1, Person p2)
+        {
+            if (p1.StateFileNumber == null || p2.StateFileNumber == null) return false;
+            return true;
+        }
+
+        private bool checkIfNSN(Person p1, Person p2)
+        {
+            if (p1 is Adult || p2 is Adult)
+            {
+                return false;
+            }
+            Child c1 = (Child)p1;
+            Child c2 = (Child)p2;
+            if (c1.NewbornScreeningNumber == null || c2.NewbornScreeningNumber == null) return false;
+            return true;
+        }
+
+        private bool checkForTwoNames(Person p1, Person p2)
+        {
+            int score = 3;
+            if (p1.FirstName == null || p2.FirstName == null)
+                score--;
+            if (p1.MiddleName == null || p2.MiddleName == null)
+                score--;
+            if (p1.LastName == null || p2.LastName == null)
+                score--;
+            if (score >= 2) return true;
+            return false;
+        }
+
+        public int MatchMethod1(Person p1, Person p2)
+        {
+            //if (checkType(p1, p2) == -1) return -1;
+            if (checkIfSSN(p1, p2))
+            {
+                if (p1.SocialSecurityNumber == p2.SocialSecurityNumber &&
+                    p1.BirthYear == p2.BirthYear &&
+                    p1.BirthMonth == p2.BirthMonth &&
+                    p1.BirthDay == p2.BirthDay)
+                    return 1;
+            }
+            else if (checkIfSFN(p1, p2))
+            {
+                if (p1.StateFileNumber == p2.StateFileNumber &&
+                    p1.BirthYear == p2.BirthYear &&
+                    p1.BirthMonth == p2.BirthMonth &&
+                    p1.BirthDay == p2.BirthDay)
+                    return 1;
+            }
+            else if (checkIfNSN(p1, p2))
+            {
+                Child c1 = (Child)p1;
+                Child c2 = (Child)p2;
+                if (c1.StateFileNumber == c2.StateFileNumber &&
+                    p1.BirthYear == p2.BirthYear &&
+                    p1.BirthMonth == p2.BirthMonth &&
+                    p1.BirthDay == p2.BirthDay)
+                    return 1;
+            }
+            return 0;
+        }
+
+
+        public int MatchMethod2(Person p1, Person p2)
+        {
+            if (checkForTwoNames(p1, p2) == false) return 0;
+            else if (checkIfSSN(p1, p2))
+            {
+                if (p1.SocialSecurityNumber == p2.SocialSecurityNumber &&
+                    p1.FirstName == p2.FirstName &&
+                    p1.MiddleName == p2.MiddleName &&
+                    p1.LastName == p2.LastName)
+                    return 1;
+            }
+            else if (checkIfSFN(p1, p2))
+            {
+                if (p1.StateFileNumber == p2.StateFileNumber &&
+                    p1.FirstName == p2.FirstName &&
+                    p1.MiddleName == p2.MiddleName &&
+                    p1.LastName == p2.LastName)
+                    return 1;
+            }
+            else if (checkIfNSN(p1, p2))
+            {
+                if (p1.StateFileNumber == p2.StateFileNumber &&
+                    p1.FirstName == p2.FirstName &&
+                    p1.MiddleName == p2.MiddleName &&
+                    p1.LastName == p2.LastName)
+                    return 1;
+            }
+            return 0;
+        }
+
+        public int MatchMethod3(Person p1, Person p2)
+        {
+            if (checkForTwoNames(p1, p2) == false) return 0;
+            else if (p1.Gender == p2.Gender &&
+                    p1.FirstName == p2.FirstName &&
+                    p1.MiddleName == p2.MiddleName &&
+                    p1.LastName == p2.LastName &&
+                    p1.BirthYear == p2.BirthYear &&
+                    p1.BirthMonth == p2.BirthMonth &&
+                    p1.BirthDay == p2.BirthDay)
+                return 1;
+            return 0;
+        }
+
+        public int MatchMethod4(Person p1, Person p2)
+        {
+            return 0;
+        }
+
+        public int MatchMethod5(Person p1, Person p2)
+        {
+            return 0;
+        }
+    }
+}
--- Assignment1/Processor.cs
+++ Assignment1/Processor.cs
@@ -1,7 +1,7 @@
 ﻿using Assignment1.IO;
-using System.Collections;
 using System.Collections.Generic;
 using System;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Assignment1
 {
@@ -9,11 +9,6 @@ namespace Assignment1
     {
         public ImportExporterStrategy StorageStrategy { get; set; }
         public string InputFileName { get; set; }
-        public int NumberOfPeriods { get; set; }
-        public int BeginningTotal { get; private set; }
-        public int EndingTotal { get; private set; }
-        public int InterestPaid { get; private set; }
-        public int FeesCollected { get; private set; }
 
         public void Run()
         {
@@ -22,41 +17,50 @@ namespace Assignment1
             personList.Load(InputFileName);
 
             int index = 0;
-
+            var matcher = new Matcher();
             //Begin to look for matches, iterate through all object in list
-            Console.WriteLine(personList.GetSize() + " - Number of objects");
+            //Console.WriteLine(personList.GetSize() + " - Number of objects");
             foreach (Person person1 in personList)
             {
 
                 List<Person> restOfList = personList.GetSublist(index+1, (personList.GetSize()-1) - index);
                 foreach (Person person2 in restOfList)
                 {
-                    Console.WriteLine("Compare Person #" + person1.ObjectId + " and Person #" + person2.ObjectId);
+                    int match = matcher.MatchMethod1(person1, person2);
+                    match += matcher.MatchMethod2(person1, person2);
+                    match += matcher.MatchMethod3(person1, person2);
+
+                    if (match >= 1)
+                        Console.WriteLine("Match between Person #" + person1.ObjectId +" and  #" + person2.ObjectId);
+
+
+
+                    //Console.WriteLine("Compare Person #" + person1.ObjectId + " and Person #" + person2.ObjectId);
                 }
                 Console.WriteLine("");
 
                 index++;
 
                 /* Print contents of each object in personList
-                Console.WriteLine("Person #" + person.ObjectId +
-                                    "\n\tState File Number: " + person.StateFileNumber +
-                                    "\n\tSocial Security Number: " + person.SocialSecurityNumber +
-                                    "\n\tFirst Name: " + person.FirstName +
-                                    "\n\tMiddle Name: " + person.MiddleName +
-                                    "\n\tLast Name: " + person.LastName +
-                                    "\n\tBirth Day: " + person.BirthDay +
-                                    "\n\tBirth Month: " + person.BirthMonth +
-                                    "\n\tBirth Year: " + person.BirthYear +
-                                    "\n\tGender: " + person.Gender);
-                if (person is Adult)
+                Console.WriteLine("Person #" + person1.ObjectId +
+                                    "\n\tState File Number: " + person1.StateFileNumber +
+                                    "\n\tSocial Security Number: " + person1.SocialSecurityNumber +
+                                    "\n\tFirst Name: " + person1.FirstName +
+                                    "\n\tMiddle Name: " + person1.MiddleName +
+                                    "\n\tLast Name: " + person1.LastName +
+                                    "\n\tBirth Day: " + person1.BirthDay +
+                                    "\n\tBirth Month: " + person1.BirthMonth +
+                                    "\n\tBirth Year: " + person1.BirthYear +
+                                    "\n\tGender: " + person1.Gender);
+                if (person1 is Adult)
                 {
-                    Adult adult = (Adult)person;
+                    Adult adult = (Adult)person1;
                     Console.WriteLine("\tPhone Number 1:" + adult.Phone1 +
                                         "\n\tPhone Number 2:" + adult.Phone2);
                 }
-                else if (person is Child)
+                else if (person1 is Child)
                 {
-                    Child child = (Child)person;
+                    Child child = (Child)person1;
                     Console.WriteLine("\tNewborn Screening Number:" + child.NewbornScreeningNumber +
                                         "\n\tIs Part of a Multiple Birth:" + child.IsPartOfMultipleBirth +
                                         "\n\tBirth Order:" + child.BirthOrder +
